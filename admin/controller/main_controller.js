848f91f1a0ba6de2ebc3b76d2f3117cb5a093581
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-07-01 18:15:22 +0530

    - added seed login and logout

--- admin/controller/main_controller.js
+++ admin/controller/main_controller.js
@@ -1,4 +1,22 @@
-app.controller("Main_Controller",function($scope){
+app.controller("Main_Controller",function($scope,$state){
   console.log(121212121212);
-  $scope.is_loggedin = true;
+  $scope.is_loggedin = false;
+
+  /*
+  * function : signIn
+  */
+  $scope.signIn = function(){
+    // call the web service to make signIn
+
+    // redirec to the dashboard
+    $scope.is_loggedin = true;
+    $state.go("dashboard");
+  }
+  $scope.signOut = function(){
+    // call the web service to make signOut and remove the token
+
+    // redirec to the login page
+    $scope.is_loggedin = false;
+    $state.go("login");
+  }
 });
