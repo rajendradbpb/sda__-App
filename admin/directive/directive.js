b106ab8467b1bcbd2a07336ab65f2df5e78b0d7f
shannsoft <santoshmajhi99@gmail.com>
2016-07-09 18:06:18 +0530

    updated height

--- admin/directive/directive.js
+++ admin/directive/directive.js
@@ -12,3 +12,15 @@ app.directive('fileModel', ['$parse', function ($parse) {
         }
     };
 }]);
+app.directive('updateHeight',function () {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            $ta = element;
+            var window_height = $(window).height();
+            $ta.css({
+              'min-height':window_height - 100+'px'
+            })
+        }
+    };
+});
